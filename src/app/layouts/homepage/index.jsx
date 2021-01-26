d3bf80f62fb56e43ff164ffd973ec3897e641a31
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-01-26 23:53:41 +0100

    refactor: remove whatsapp button

--- src/app/layouts/homepage/index.jsx
+++ src/app/layouts/homepage/index.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import style from "./style.scss"
+import React from "react";
+import style from "./style.scss";
 import { ProfilePicture } from "../../components/profile-picture";
 import profilePicture from "../../assets/pictures/profile-picture.svg";
 import { TypedQuote } from "../../components/typed-quote";
@@ -7,26 +7,23 @@ import { Title } from "../../components/title";
 import { GithubSocialButton } from "../../components/social-button/github";
 import { EmailSocialButton } from "../../components/social-button/email";
 import { LinkedinSocialButton } from "../../components/social-button/linkedin";
-import { WhatsappSocialButton } from "../../components/social-button/whatsapp";
-
 
 export function HomePage() {
-  return <>
-    <div className={style.content}>
-      <div className={style.background}/>
-      <div className={style.whiteFilter}/>
-      <ProfilePicture image={profilePicture} size="150px"/>
-      <Title>Guillaume Quittet</Title>
-      <TypedQuote>
-        A lovely passionate developer who want to make your ideas to be alive.
-      </TypedQuote>
-      <div className={style.buttonIcons}>
-        <GithubSocialButton username="gquittet"/>
-        <LinkedinSocialButton username="gquittet"/>
-        <WhatsappSocialButton phone="32472084236"/>
-        <EmailSocialButton email="guillaume.quittet@gmail.com"/>
+  return (
+    <>
+      <div className={style.content}>
+        <div className={style.background} />
+        <div className={style.whiteFilter} />
+        <ProfilePicture image={profilePicture} size="150px" />
+        <Title>Guillaume Quittet</Title>
+        <TypedQuote>A lovely passionate developer who want to make your ideas to be alive.</TypedQuote>
+        <div className={style.buttonIcons}>
+          <GithubSocialButton username="gquittet" />
+          <LinkedinSocialButton username="gquittet" />
+          <EmailSocialButton email="guillaume.quittet@gmail.com" />
+        </div>
+        <footer>Made with ❤ by Guillaume Quittet</footer>
       </div>
-      <footer>Made with ❤ by Guillaume Quittet</footer>
-    </div>
-  </>
+    </>
+  );
 }
